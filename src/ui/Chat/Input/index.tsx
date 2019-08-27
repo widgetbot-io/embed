4fb6a8d8d724e5774eb1c36dbeaa6f656d1db7bf
LightningsFury <light@memeware.net>
2019-08-27 15:01:07 +0100

    Made it the text input when not logged in look more legit

--- src/ui/Chat/Input/index.tsx
+++ src/ui/Chat/Input/index.tsx
@@ -1,29 +1,29 @@
-import * as React from 'react'
+import * as React from "react";
 
-import { Root, Textarea } from './elements'
-import Suggestions from './Suggestions'
-import Channels from './suggestions/channels'
-import Commands from './suggestions/commands'
-import Emojis from './suggestions/emojis'
-import Mentions from './suggestions/mentions'
-import extractQuery from './utils/extractQuery'
-import injectValue from './utils/injectValue'
-import { ChatProps } from '../Chat';
+import { Root, Textarea } from "./elements";
+import Suggestions from "./Suggestions";
+import Channels from "./suggestions/channels";
+import Commands from "./suggestions/commands";
+import Emojis from "./suggestions/emojis";
+import Mentions from "./suggestions/mentions";
+import extractQuery from "./utils/extractQuery";
+import injectValue from "./utils/injectValue";
+import { ChatProps } from "../Chat";
 import { inject, observer } from "mobx-react";
 import { AuthStore } from "../../../stores/auth";
 
 interface Props extends ChatProps {
-  innerRef?: (textarea: HTMLTextAreaElement) => void
-  innerProps?: React.InputHTMLAttributes<HTMLTextAreaElement>
-  AuthStore?: AuthStore
-  onChange?: Function
-  onKeyPress?: Function
-  onSubmit?: Function
+  innerRef?: (textarea: HTMLTextAreaElement) => void;
+  innerProps?: React.InputHTMLAttributes<HTMLTextAreaElement>;
+  AuthStore?: AuthStore;
+  onChange?: Function;
+  onKeyPress?: Function;
+  onSubmit?: Function;
 }
 
-export const handlers = [Emojis, Mentions, Commands, Channels]
+export const handlers = [Emojis, Mentions, Commands, Channels];
 
-@inject('AuthStore')
+@inject("AuthStore")
 @observer
 class MagicTextarea extends React.Component<Props> {
   initialState = {
@@ -34,19 +34,19 @@ class MagicTextarea extends React.Component<Props> {
     handler: null,
 
     query: null
-  }
+  };
 
-  state = this.initialState
-  resetState = () => this.setState(this.initialState)
+  state = this.initialState;
+  resetState = () => this.setState(this.initialState);
 
-  getValue = () => this.textarea.value
-  textarea: HTMLTextAreaElement
-  suggestions: Suggestions
+  getValue = () => this.textarea.value;
+  textarea: HTMLTextAreaElement;
+  suggestions: Suggestions;
 
   onChange(value) {
-    const { onChange } = this.props
+    const { onChange } = this.props;
 
-    onChange && onChange(value)
+    onChange && onChange(value);
   }
 
   render() {
@@ -55,68 +55,68 @@ class MagicTextarea extends React.Component<Props> {
         <Textarea
           {...this.props.innerProps}
           innerRef={ref => {
-            const { innerRef } = this.props
+            const { innerRef } = this.props;
 
-            this.textarea = ref
-            innerRef && innerRef(ref)
+            this.textarea = ref;
+            innerRef && innerRef(ref);
           }}
           onChange={event => this.onChange(event.target.value)}
           onClick={this.resetState}
           onKeyDown={event => {
             switch (event.key) {
-              case 'ArrowUp':
-              case 'ArrowDown':
-                if (!this.suggestions) return
+              case "ArrowUp":
+              case "ArrowDown":
+                if (!this.suggestions) return;
 
-                this.suggestions.traverseSuggestions(event.key === 'ArrowDown')
-                event.preventDefault()
-                return
-              case 'Tab':
-                if (!this.suggestions) return
+                this.suggestions.traverseSuggestions(event.key === "ArrowDown");
+                event.preventDefault();
+                return;
+              case "Tab":
+                if (!this.suggestions) return;
 
-                this.suggestions.selectSuggestion()
-                event.preventDefault()
-                return
-              case 'Enter':
+                this.suggestions.selectSuggestion();
+                event.preventDefault();
+                return;
+              case "Enter":
                 if (this.state.showSuggestions) {
-                  this.suggestions && this.suggestions.selectSuggestion()
-                  event.preventDefault()
-                  return
+                  this.suggestions && this.suggestions.selectSuggestion();
+                  event.preventDefault();
+                  return;
                 }
 
                 if (!event.shiftKey) {
-                  const { onSubmit, onChange } = this.props
+                  const { onSubmit, onChange } = this.props;
 
                   // Submit
-                  onSubmit && onSubmit(this.textarea.value)
-                  onChange && onChange('')
-                  event.preventDefault()
+                  onSubmit && onSubmit(this.textarea.value);
+                  onChange && onChange("");
+                  event.preventDefault();
                 }
 
-                return
-              case 'Escape':
-                this.resetState()
-                break
+                return;
+              case "Escape":
+                this.resetState();
+                break;
               default:
-                if (this.props.onKeyPress) this.props.onKeyPress(event)
-                break
+                if (this.props.onKeyPress) this.props.onKeyPress(event);
+                break;
             }
           }}
           onInput={() => {
-            const { query, ...rest } = extractQuery(this.textarea)
-            this.setState({ ...rest, showSuggestions: false })
+            const { query, ...rest } = extractQuery(this.textarea);
+            this.setState({ ...rest, showSuggestions: false });
 
             // Find a parser for the selected query
             for (const handler of handlers) {
-              const parsedQuery = handler.extract(query, this.textarea)
+              const parsedQuery = handler.extract(query, this.textarea);
 
-              if (typeof parsedQuery === 'string') {
+              if (typeof parsedQuery === "string") {
                 this.setState({
                   showSuggestions: true,
                   query: parsedQuery,
                   handler
-                })
-                break
+                });
+                break;
               }
             }
           }}
@@ -128,7 +128,7 @@ class MagicTextarea extends React.Component<Props> {
             query={this.state.query}
             handler={this.state.handler}
             onSelect={suggestion => {
-              const { handler, leftIndex, caretPosition } = this.state
+              const { handler, leftIndex, caretPosition } = this.state;
 
               if (suggestion) {
                 const text = injectValue({
@@ -136,18 +136,29 @@ class MagicTextarea extends React.Component<Props> {
                   value: handler.toString(suggestion),
                   leftIndex,
                   caretPosition
-                })
+                });
 
-                this.onChange(text)
+                this.onChange(text);
               }
 
-              this.resetState()
+              this.resetState();
             }}
           />
         )}
       </Root>
-    ) : null;
+    ) : (
+      <p
+        style={{
+          opacity: 0.4,
+          marginTop: "10px",
+          marginBottom: 0,
+          marginLeft: "10px"
+        }}
+      >
+        You do not have permission to send messages in this channel.
+      </p>
+    );
   }
 }
 
-export default MagicTextarea
+export default MagicTextarea;
