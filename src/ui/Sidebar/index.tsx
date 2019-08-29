cd8be596d80257657e8ed41b0e392559109e5c30
Daave <daave@aaathats3as.com>
2019-08-29 10:05:01 +0100

    Convert to class

--- src/ui/Sidebar/index.tsx
+++ src/ui/Sidebar/index.tsx
@@ -5,17 +5,27 @@ import Channels from './Channels'
 import { Root } from './elements'
 import Header from './Header'
 import Panel from './Panel'
-import { observer } from 'mobx-react'
+import {inject, observer} from 'mobx-react'
 import { store } from '@models'
+import {AuthStore} from "@store/auth";
+import {NEW_MESSAGES, useRouter} from "@hooks";
+import {Subscription} from "react-apollo";
 
-const Sidebar = observer(() => {
-  return (
-    <Root visible={store.sidebar.isOpen} className="sidebar">
-      <Header />
-      <Channels />
-      <Panel />
-    </Root>
-  )
-});
+interface Props {
+    AuthStore?: AuthStore
+}
+
+@observer
+class Sidebar extends React.PureComponent<{}> {
+    render() {
+        return (
+            <Root visible={store.sidebar.isOpen} className="sidebar">
+                <Header />
+                <Channels />
+                <Panel />
+            </Root>
+        )
+    }
+}
 
 export default Sidebar
