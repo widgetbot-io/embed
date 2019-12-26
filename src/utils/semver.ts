139f4c06738a9f7c2c1d3f503bb4e6c969f5f6a4
Daave <daave@widgetbot.io>
2019-12-26 18:36:30 +0000

    Unify auth and add lastUpdate for future majors

---
+++ src/utils/semver.ts
@@ -0,0 +1,32 @@
+export class Semver {
+	private static split(ver: string): string[] {
+		const c = ver.replace(/^v/, '');
+		const index = c.indexOf('-') === -1 ? c.length : c.indexOf('-');
+
+		return [
+			...c.substring(0, index).split('.'),
+			...c.substring(index + 1)
+		];
+	}
+
+	public static sameMinor(oldVersion: string, newVersion: string): boolean {
+		const s1 = Semver.split(oldVersion);
+		const s2 = Semver.split(newVersion);
+
+		return s1[1] === s2[1];
+	}
+
+	public static newMinor(oldVersion: string, newVersion: string): boolean {
+		const s1 = Semver.split(oldVersion);
+		const s2 = Semver.split(newVersion);
+
+		return s1[1] < s2[1];
+	}
+
+	public static newMinorOrMajor(oldVersion: string = 'v0.0.0', newVersion: string = 'v0.0.0'): boolean {
+		const s1 = Semver.split(oldVersion);
+		const s2 = Semver.split(newVersion);
+
+		return (s1[1] < s2[1]) || (s1[2] < s2[1]);
+	}
+}
