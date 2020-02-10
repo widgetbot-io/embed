448b7f04d43a561ad73e57e9ae1b6126dd5b9a25
IceeMC <johnburke915@gmail.com>
2020-02-09 18:24:15 -0800

    react context + hooks

--- src/lib/Locale.tsx
+++ src/lib/Locale.tsx
@@ -1,62 +1,88 @@
-import React from "react";
-import { AuthStore } from "@store/auth";
+import React, { createContext, useContext, useReducer } from "react";
+import { AuthStore } from "../stores/auth";
 import {inject} from "mobx-react";
 
 interface Props {
     AuthStore?: AuthStore;
+    cache?: [[string, { [key: string]: string; }]]
 }
 
+interface LocaleContextState {
+    cur: string;
+    cache: { [key: string]: { [key: string]: string; } };
+}
+
+const LocaleContext = createContext({});
+
+const reducer = (state: LocaleContextState, action: { type: string; body: any }) => {
+    switch (action.type) {
+        case "SET_LOCALE": return { ...state, cur: action.body };
+        default: return state;
+    }
+};
+
+const Init = ({ children, cur, cache }) => {
+    const [state, dispatch] = useReducer(reducer, { cur, cache });
+    return <LocaleContext.Provider value={{ state, dispatch }}>{children}</LocaleContext.Provider>;
+};
+
 @inject("AuthStore")
-export class Locale extends React.Component<Props> {
-    public static cache: { [key: string]: { [key: string]: string } } = JSON.parse(window.localStorage.getItem("locales") || '{}');
-    public static cur: string;
+export class Locale extends React.Component<Props, { cache: { [key: string]: { [key: string]: string; } } }> {
 
     constructor(props) {
         super(props);
-        Locale.cur = this.props.AuthStore.locale;
-        Locale.init().then(() => {});
+        this.state = { cache: {} };
     }
 
-    static translate(key: string, replacements?: { [key: string]: any; }): string {
-        let lang: any = Locale.cache[Locale.cur], content: string;
+    async componentDidMount() {
+        this.setState({
+            cache: {
+                en: (await import("../locales/en.json")) as any
+            }
+        });
+    }
 
+    static translate(key: string, replacements?: { [key: string]: string; }) {
+        const { state: { cur, cache } }: any = useContext(LocaleContext);
+        let lang: any = cache[cur], content: string;
         if (!lang) {
-            if (Locale.cache['en'] && Locale.cache['en'][key]) {
-                lang = Locale.cache['en'];
+            if (cache["en"] && cache["en"][key]) {
+                lang = cache["en"];
             } else {
                 return key;
             }
         }
+        if (!lang[key]) return key;
         if (!replacements) return lang[key];
         content = lang[key];
+        return null;
+    };
 
-        for (const replace in replacements) {
-            const use: any = replacements[replace];
-            content = content.replace(new RegExp(`{${replace}}`, 'g'), use);
-        }
-
-        return content;
-    }
-
-    private static async init() {
-        const temp = {};
-        // @ts-ignore
-        temp["en"] = (await import("../locales/en.json"));
-
-        // When adding new locales, do it ABOVE this line!
-        const cached = Object.keys(Locale.cache).length < 1 ? null : JSON.stringify(Locale.cache);
-        if (!cached || cached !== JSON.stringify(temp)) {
-            Locale.cache = temp;
-            window.localStorage.setItem("locales", JSON.stringify(temp));
-        }
-    }
-
-    static getKeys(): String[] {
-        return Object.keys(Locale.cache);
-    }
+    // static translate(key: string, replacements?: { [key: string]: any; }): string {
+    //     // let lang: any = Locale.cache[Locale.cur], content: string;
+    //     //
+    //     // if (!lang) {
+    //     //     if (Locale.cache['en'] && Locale.cache['en'][key]) {
+    //     //         lang = Locale.cache['en'];
+    //     //     } else {
+    //     //         return key;
+    //     //     }
+    //     // }
+    //     // if (!replacements) return lang[key];
+    //     // content = lang[key];
+    //     //
+    //     // for (const replace in replacements) {
+    //     //     const use: any = replacements[replace];
+    //     //     content = content.replace(new RegExp(`{${replace}}`, 'g'), use);
+    //     // }
+    //     //
+    //     // return content;
+    //     return key;
+    // }
 
     render() {
-        return this.props.children;
+        if (Object.keys(this.state.cache).length < 1) return null;
+        return <Init cur={this.props.AuthStore.locale} cache={this.state.cache}>{this.props.children}</Init>;
     }
 
 }
