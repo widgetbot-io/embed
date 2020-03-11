727229aa4e10d2e527d05ca11419509c27d89aa3
Daave <daave@widgetbot.io>
2020-03-11 22:57:18 +0000

    Add v to version and change WS detection

--- src/lib/apollo/link/index.ts
+++ src/lib/apollo/link/index.ts
@@ -9,7 +9,7 @@ import wsLink from './websocket'
 import { url } from "@lib/env";
 
 const DEVELOPMENT = process.env.NODE_ENV === 'development';
-console.log(`${location.protocol === 'https:' ? 'wss' : 'ws'}://${url}/api/graphql`);
+console.log(`${url.includes('127.0.0.1') ? 'ws' : 'wss'}://${url}/api/graphql`);
 
 
 const link = ApolloLink.from(
