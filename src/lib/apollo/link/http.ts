9d61b2eeaad447e68b9c003d8a8dc584b923c452
Daave <daave@widgetbot.io>
2020-04-13 12:39:37 +0100

    Log people out automatically when there is auth errors

--- src/lib/apollo/link/http.ts
+++ src/lib/apollo/link/http.ts
@@ -1,23 +1,25 @@
-import { ApolloLink } from 'apollo-link'
-import { BatchHttpLink } from 'apollo-link-batch-http'
-import { onError } from 'apollo-link-error'
+import {ApolloLink} from 'apollo-link'
+import {BatchHttpLink} from 'apollo-link-batch-http'
+import {onError} from 'apollo-link-error'
 import {url} from "@lib/env";
+import {authStore} from '@store';
 
 const httpLink = ApolloLink.from([
-  onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors)
-      graphQLErrors.map(({ message, locations, path }) =>
-        console.error(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        )
-      )
-    if (networkError) console.error(`[Network error]: ${networkError}`)
-  }),
-  new BatchHttpLink({
-    uri: url.includes('127.0.0.1') ? `http://${url}/api/graphql` : `https://${url}/api/graphql`,
-    batchInterval: 20,
-    batchMax: 2
-  })
+	onError(({graphQLErrors, networkError}) => {
+		if (graphQLErrors)
+			graphQLErrors.map(({message, locations, path}) =>
+				message === 'Please logout and login again.' ?
+					authStore.logout()
+					:
+					console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+			);
+		if (networkError) console.error(`[Network error]: ${networkError}`)
+	}),
+	new BatchHttpLink({
+		uri: url.includes('127.0.0.1') ? `http://${url}/api/graphql` : `https://${url}/api/graphql`,
+		batchInterval: 20,
+		batchMax: 2
+	})
 ].filter(Boolean) as any);
 
 export default httpLink
