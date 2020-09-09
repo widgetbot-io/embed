22857b06e2e205c66327d675e3f28955ea3b7ab4
Daave <daave@widgetbot.io>
2020-09-09 18:47:10 +0100

    Hopefully fix a load of embed issues

--- src/ui/Message/Embed/index.tsx
+++ src/ui/Message/Embed/index.tsx
@@ -1,240 +1,241 @@
-import { Twemoji } from '@ui/shared/Emoji/emoji'
-import { parseAllowLinks, parseEmbedTitle } from '@ui/shared/markdown/render'
-import { ThemeProvider } from 'emotion-theming'
+import {Twemoji} from '@ui/shared/Emoji/emoji'
+import {parseAllowLinks, parseEmbedTitle} from '@ui/shared/markdown/render'
+import {ThemeProvider} from 'emotion-theming'
 import * as Moment from 'moment'
 import * as React from 'react'
 
-import { Content, Root, Title, Wrapper, Provider, VideoIframe, Gifv } from './elements'
-import { Author, AuthorIcon, AuthorName } from './elements/author'
-import { Description } from './elements/description'
-import { Field, FieldName, Fields, FieldValue } from './elements/fields'
-import { Footer, FooterIcon, FooterText } from './elements/footer'
-import { Image } from './elements/media'
-import { Thumbnail } from './elements/thumbnail'
-import { Video } from '@ui/Message/elements'
+import {Content, Root, Title, Wrapper, Provider, VideoIframe, Gifv} from './elements'
+import {Author, AuthorIcon, AuthorName} from './elements/author'
+import {Description} from './elements/description'
+import {Field, FieldName, Fields, FieldValue} from './elements/fields'
+import {Footer, FooterIcon, FooterText} from './elements/footer'
+import {Image} from './elements/media'
+import {Thumbnail} from './elements/thumbnail'
+import {Video} from '@ui/Message/elements'
+import {Embed_author, Embed_footer, Embed_image, Embed_thumbnail, TextMessage_embeds} from "@generated";
 
 // TODO: Refactor / cleanup
 
 const parseEmojis = text =>
-  text && typeof text === 'string' ? (
-    <Twemoji resolveNames>{text}</Twemoji>
-  ) : text instanceof Array ? (
-    text.map(
-      (part, i) =>
-        typeof part === 'string' ? (
-          <Twemoji resolveNames key={i + part}>
-            {part}
-          </Twemoji>
-        ) : (
-          part
+    text && typeof text === 'string' ? (
+        <Twemoji resolveNames>{text}</Twemoji>
+    ) : text instanceof Array ? (
+        text.map(
+            (part, i) =>
+                typeof part === 'string' ? (
+                    <Twemoji resolveNames key={i + part}>
+                        {part}
+                    </Twemoji>
+                ) : (
+                    part
+                )
         )
+    ) : (
+        text
     )
-  ) : (
-    text
-  )
 
-const Link = ({ children, ...props }) => (
-  <a target="_blank" rel="noreferrer" {...props}>
-    {children}
-  </a>
+const Link = ({children, ...props}) => (
+    <a target="_blank" rel="noreferrer" {...props}>
+        {children}
+    </a>
 )
 
-const EmbedTitle = ({ title, url }) =>
-  title ? (
-    url ? (
-      <Title>
-        <Link href={url}>{parseEmojis(parseEmbedTitle(title))}</Link>
-      </Title>
-    ) : (
-      <Title>{parseEmojis(parseEmbedTitle(title))}</Title>
-    )
-  ) : null
-
-const EmbedProvider = ({ name, url }) =>
-  name ? (
-    url ? (
-      <Provider>
-        <Link href={url}>{parseEmojis(parseEmbedTitle(name))}</Link>
-      </Provider>
-    ) : (
-      <Provider>{parseEmojis(parseEmbedTitle(name))}</Provider>
-    )
-  ) : null
-
-const EmbedDescription = ({ content }) =>
-  content ? (
-    <Description>{parseEmojis(parseAllowLinks(content))}</Description>
-  ) : null
-
-const EmbedVideo = ({ url, height, width }) =>
-  url ? (
-    <VideoIframe src={url} height={height} width={width}></VideoIframe>
-  ) : null
-
-const EmbedAuthor = ({ name, url, proxyIconURL }) => {
-  if (!name) {
-    return null
-  }
-
-  let authorName
-  if (name) {
-    authorName = <AuthorName>{name}</AuthorName>
-    if (url) {
-      authorName = (
-        <AuthorName>
-          <Link href={url}>{name}</Link>
-        </AuthorName>
-      )
-    }
-  }
-
-  const authorIcon = proxyIconURL ? <AuthorIcon src={proxyIconURL} /> : null
-
-  return (
-    <Author>
-      {authorIcon}
-      {parseEmojis(authorName)}
-    </Author>
-  )
-}
-
-const EmbedField = ({ name, value, inline }) => {
-  if (!name && !value) {
-    return null
-  }
-
-  const fieldName = name ? (
-    <FieldName>{parseEmojis(parseEmbedTitle(name))}</FieldName>
-  ) : null
-  const fieldValue = value ? (
-    <FieldValue>{parseEmojis(parseAllowLinks(value))}</FieldValue>
-  ) : null
-
-  return (
-    <Field inline={inline}>
-      {fieldName}
-      {fieldValue}
-    </Field>
-  )
-}
-
-const EmbedThumbnail = ({ type, proxyURL, height, width }) =>
-  type === 'video' ? null :
-    proxyURL ? (
-      <Thumbnail
-        src={proxyURL}
-        height={height}
-        width={width}
-        maxWidth={/^article|image$/.test(type) ? 400 : 80}
-        maxHeight={/^article|image$/.test(type) ? 300 : 80}
-      />
+const EmbedTitle = ({title, url}) =>
+    title ? (
+        url ? (
+            <Title>
+                <Link href={url}>{parseEmojis(parseEmbedTitle(title))}</Link>
+            </Title>
+        ) : (
+            <Title>{parseEmojis(parseEmbedTitle(title))}</Title>
+        )
     ) : null
 
-const EmbedImage = ({ proxyURL, height, width }) =>
-  proxyURL ? (
-    <span>
-      <Thumbnail
-        rich
-        src={proxyURL}
-        height={height}
-        width={width}
-        maxWidth={400}
-        maxHeight={300}
-      />
-    </span>
-  ) : null
+const EmbedProvider = ({name, url}) =>
+    name ? (
+        url ? (
+            <Provider>
+                <Link href={url}>{parseEmojis(parseEmbedTitle(name))}</Link>
+            </Provider>
+        ) : (
+            <Provider>{parseEmojis(parseEmbedTitle(name))}</Provider>
+        )
+    ) : null
 
-const EmbedFooter = ({ timestamp, text, proxyIconURL }) => {
-  if (!text && !timestamp) {
-    return null
-  }
+const EmbedDescription = ({content}) =>
+    content ? (
+        <Description>{parseEmojis(parseAllowLinks(content))}</Description>
+    ) : null
 
-  // pass null, since undefined will make moment(...) return the current date/time
-  let time = Moment(timestamp !== undefined ? timestamp * 1000 : null)
+const EmbedVideo = ({url, height, width}) =>
+    url ? (
+        <VideoIframe src={url} height={height} width={width}/>
+    ) : null
 
-  const footerText = [text, time.isValid() ? time.calendar() : null]
-    .filter(Boolean)
-    .join(' • ')
+const EmbedAuthor = ({name, url}: Embed_author) => {
+    if (!name) {
+        return null
+    }
 
-  const footerIcon =
-    text && proxyIconURL ? <FooterIcon src={proxyIconURL} /> : null
+    let authorName
+    if (name) {
+        authorName = <AuthorName>{name}</AuthorName>
+        if (url) {
+            authorName = (
+                <AuthorName>
+                    <Link href={url}>{name}</Link>
+                </AuthorName>
+            )
+        }
+    }
 
-  return (
-    <Footer>
-      {footerIcon}
-      <FooterText>{parseEmojis(footerText)}</FooterText>
-    </Footer>
-  )
+    const authorIcon = url ? <AuthorIcon src={url}/> : null
+
+    return (
+        <Author>
+            {authorIcon}
+            {parseEmojis(authorName)}
+        </Author>
+    )
 }
 
-const EmbedFields = ({ fields }) => {
-  if (!fields) {
-    return null
-  }
+const EmbedField = ({name, value, inline}) => {
+    if (!name && !value) {
+        return null
+    }
 
-  return <Fields>{fields.map((f, i) => <EmbedField key={i} {...f} />)}</Fields>
+    const fieldName = name ? (
+        <FieldName>{parseEmojis(parseEmbedTitle(name))}</FieldName>
+    ) : null
+    const fieldValue = value ? (
+        <FieldValue>{parseEmojis(parseAllowLinks(value))}</FieldValue>
+    ) : null
+
+    return (
+        <Field inline={inline}>
+            {fieldName}
+            {fieldValue}
+        </Field>
+    )
+}
+
+const EmbedThumbnail = ({url, height, width, type}: { height: number | null, width: number | null, url: string | null, type: string }) =>
+    type === 'video' ? null :
+        url ? (
+            <Thumbnail
+                src={url}
+                height={height}
+                width={width}
+                maxWidth={/^article|image$/.test(type) ? 400 : 80}
+                maxHeight={/^article|image$/.test(type) ? 300 : 80}
+            />
+        ) : null
+
+const EmbedImage = ({url, height, width}: Embed_image) =>
+    url ? (
+        <span>
+      <Thumbnail
+          rich
+          src={url}
+          height={height}
+          width={width}
+          maxWidth={400}
+          maxHeight={300}
+      />
+    </span>
+    ) : null
+
+const EmbedFooter = ({timestamp, text, url}: { timestamp: number, url: string | null, text: string }) => {
+    if (!text && !timestamp) {
+        return null
+    }
+
+    // pass null, since undefined will make moment(...) return the current date/time
+    let time = Moment(timestamp !== undefined ? timestamp * 1000 : null)
+
+    const footerText = [text, time.isValid() ? time.calendar() : null]
+        .filter(Boolean)
+        .join(' • ')
+
+    const footerIcon =
+        text && url ? <FooterIcon src={url}/> : null
+
+    return (
+        <Footer>
+            {footerIcon}
+            <FooterText>{parseEmojis(footerText)}</FooterText>
+        </Footer>
+    )
+}
+
+const EmbedFields = ({fields}) => {
+    if (!fields) {
+        return null
+    }
+
+    return <Fields>{fields.map((f, i) => <EmbedField key={i} {...f} />)}</Fields>
 }
 
 const Embed = ({
-  color,
-  author,
-  title,
-  url,
-  description,
-  fields,
-  thumbnail,
-  image,
-  timestamp,
-  footer,
-  provider,
-  video,
-  ...embed
-}) =>
-  embed.type === 'gifv' ? (
-    <Gifv autoPlay loop muted
-    src={video.proxyURL || video.url}
-    width={+video.width}
-    height={+video.height}
-    />
-  ) : embed.type === 'image' ? (
-    <Image
-      src={thumbnail.proxyURL}
-      width={+thumbnail.width}
-      height={+thumbnail.height}
-    />
-  ) : embed.type === 'video' && !thumbnail ? (
-    <Video controls
-    src={video.proxyURL || video.url}
-    width={+video.width}
-    height={+video.height}
-    />
-  ) : (
-    <ThemeProvider
-      theme={theme => ({
-        ...theme,
-        embed
-      })}
-    >
-      <Root>
-        {/* @ts-expect-error */}
-        <Wrapper color={color}>
-          <Content>
-            <div>
-              <EmbedProvider {...provider}/>
-              <EmbedAuthor {...author} />
-              <EmbedTitle title={title} url={url} />
-              {embed.type === 'video' ? 
-                <EmbedVideo {...video} />:
-                <EmbedDescription content={description} />}
-              <EmbedFields fields={fields} />
-            </div>
-            <EmbedThumbnail type={embed.type} {...thumbnail} />
-          </Content>
-          <EmbedImage {...image} />
-          <EmbedFooter timestamp={timestamp} {...footer} />
-        </Wrapper>
-      </Root>
-    </ThemeProvider>
-  )
+   color,
+   author,
+   title,
+   url,
+   description,
+   fields,
+   thumbnail,
+   image,
+   timestamp,
+   footer,
+   provider,
+   video,
+   ...embed
+}: TextMessage_embeds) =>
+    embed.type === 'gifv' ? (
+        <Gifv autoPlay loop muted
+              src={video.url}
+              width={+video.width}
+              height={+video.height}
+        />
+    ) : embed.type === 'image' ? (
+        <Image
+            src={thumbnail.url}
+            width={+thumbnail.width}
+            height={+thumbnail.height}
+        />
+    ) : embed.type === 'video' && !thumbnail ? (
+        <Video controls
+               src={video.url}
+               width={+video.width}
+               height={+video.height}
+        />
+    ) : (
+        <ThemeProvider
+            theme={theme => ({
+                ...theme,
+                embed
+            })}
+        >
+            <Root>
+                {/* @ts-expect-error */}
+                <Wrapper color={color}>
+                    <Content>
+                        <div>
+                            <EmbedProvider {...provider}/>
+                            <EmbedAuthor {...author} />
+                            <EmbedTitle title={title} url={url}/>
+                            {embed.type === 'video' ?
+                                <EmbedVideo {...video} /> :
+                                <EmbedDescription content={description}/>}
+                            <EmbedFields fields={fields}/>
+                        </div>
+                        <EmbedThumbnail type={embed.type} {...thumbnail} />
+                    </Content>
+                    <EmbedImage {...image} />
+                    <EmbedFooter timestamp={timestamp} {...footer} />
+                </Wrapper>
+            </Root>
+        </ThemeProvider>
+    )
 
 export default Embed
