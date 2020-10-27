f4f08e75df8ae6c842ebfd5a95befad189015bc3
Daave <daave@widgetbot.io>
2020-10-27 16:49:38 +0000

    Testing again

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -42,7 +42,7 @@ then
     echo "No custom server specified, using stonks.widgetbot.io"
 else
     echo "Custom server specified, changing..."
-    sed -i s/stonks.widgetbot.io/$CUSTOM_SERVER_ENDPOINT/g src/lib/env.ts
+    # sed -i s/stonks.widgetbot.io/$CUSTOM_SERVER_ENDPOINT/g src/lib/env.ts
 fi
 # --END CUSTOM ENTRYPOINT--
 
