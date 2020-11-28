5c48a4a7ee9f1dca95b47f57996f634b9963d738
Advaith <advaithj1@gmail.com>
2020-11-27 16:14:46 -0800

    fix blank channel list issue

--- src/ui/Sidebar/Channels/index.tsx
+++ src/ui/Sidebar/Channels/index.tsx
@@ -28,10 +28,31 @@ export const ChannelSwitcher = observer(() => (
 			return (
 				<Root className="channels">
 					<Selector itemID={ITEM_ID}/>
-					{generalStore.channels.map((category, i) => (
-							<Category key={i} category={category} activeChannel={channel} index={i}/>
+					{ // if there are no channels in generalStore, do a Channels query to avoid empty channel list
+					(generalStore.channels.length === 0) ? (
+					<Query<Channels, ChannelsVariables>
+						key={`guild_info:${guild}`}
+						query={CHANNELS}
+						variables={{guild}}
+						fetchPolicy='cache-and-network'
+					>
+						{({data}) => {
+							try {
+								generalStore.setChannels(categorise((data.guild.channels as any).sort((a, b) => { return a.position - b.position })))
+							} catch (_) {
+								generalStore.setChannels([])
+							}
+
+							return generalStore.channels.map((category, i) => (
+								<Category key={i} category={category} activeChannel={channel} index={i}/>
+							)
 						)
-					)}
+						}}
+					</Query>
+					// otherwise, just load from generalStore; channels will be updated by the GuildInfo query
+					) : generalStore.channels.map((category, i) => (
+						<Category key={i} category={category} activeChannel={channel} index={i}/>
+					))}
 				</Root>
 			)
 		}}
