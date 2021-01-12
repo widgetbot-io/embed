6b83b87842fb7cd65afe9478a5e2e6e4693ff3b7
Advaith <advaithj1@gmail.com>
2021-01-11 16:53:21 -0800

    add sticker reply text

--- src/ui/Message/index.tsx
+++ src/ui/Message/index.tsx
@@ -101,8 +101,13 @@ class Message extends React.PureComponent<Props, any> {
                   <RepliedUser color="#fff">{repliedMessage.author.name}</RepliedUser>
                   {repliedMessage.content
                     ? <RepliedText><Markdown>{repliedMessage.content}</Markdown></RepliedText>
+                    : repliedMessage.stickers
+                    ? <React.Fragment>
+                        <ReplySystemText>{repliedMessage.stickers[0].name} sticker</ReplySystemText>
+                        <ReplyImageIcon width="20" height="20" aria-hidden="false" viewBox="0 0 16 16"><path fillRule="evenodd" clipRule="evenodd" d="M9.20038 2.39762V5.24178C9.20038 6.10455 9.89673 6.80072 10.7597 6.80072H13.6046C13.9558 6.80072 14.1343 6.37826 13.8844 6.12835L9.87292 2.11796C9.62295 1.86806 9.20038 2.04061 9.20038 2.39762ZM10.7461 8.01794C9.22044 8.01794 7.98197 6.77947 7.98197 5.25382V2.03499H3.19561C2.53749 2.03499 1.99902 2.57346 1.99902 3.23158V12.8043C1.99902 13.4624 2.53749 14.0009 3.19561 14.0009H12.7683C13.4265 14.0009 13.9649 13.4624 13.9649 12.8043V8.01794H10.7461ZM9.80015 9C9.80015 9.99411 8.99427 10.8 8.00015 10.8C7.00604 10.8 6.20015 9.99411 6.20015 9H5.00015C5.00015 10.6569 6.3433 12 8.00015 12C9.65701 12 11.0002 10.6569 11.0002 9H9.80015Z" fill="rgba(255,255,255,.66)"></path></ReplyImageIcon>
+                      </React.Fragment>
                     : <ReplySystemText>Attachment</ReplySystemText>}
-                  {(repliedMessage.attachments.length > 0 || repliedMessage.embeds.length > 0 || repliedMessage.stickers) &&
+                  {(repliedMessage.attachments.length > 0 || repliedMessage.embeds.length > 0) &&
                     <ReplyImageIcon aria-hidden="false" width="20" height="20" viewBox="0 0 64 64"><path fill="rgba(255,255,255,.66)" d="M56 50.6667V13.3333C56 10.4 53.6 8 50.6667 8H13.3333C10.4 8 8 10.4 8 13.3333V50.6667C8 53.6 10.4 56 13.3333 56H50.6667C53.6 56 56 53.6 56 50.6667ZM22.6667 36L29.3333 44.0267L38.6667 32L50.6667 48H13.3333L22.6667 36Z"></path></ReplyImageIcon>}
                 </RepliedMessage>
                 :
@@ -218,15 +223,16 @@ class Message extends React.PureComponent<Props, any> {
                           <Embed key={i} {...e} />
                       ))}
 
-                      {message.stickers && message.stickers.map((s, i) => 
+                      {message.stickers && message.stickers.map(s => 
                         <Tooltip
+                          key={s.id}
                           placement="top"
                           overlay={<React.Fragment><StickerTooltipIcon width="16" height="16" viewBox="0 0 16 16"><path fillRule="evenodd" clipRule="evenodd" d="M9.20038 2.39762V5.24178C9.20038 6.10455 9.89673 6.80072 10.7597 6.80072H13.6046C13.9558 6.80072 14.1343 6.37826 13.8844 6.12835L9.87292 2.11796C9.62295 1.86806 9.20038 2.04061 9.20038 2.39762ZM10.7461 8.01794C9.22044 8.01794 7.98197 6.77947 7.98197 5.25382V2.03499H3.19561C2.53749 2.03499 1.99902 2.57346 1.99902 3.23158V12.8043C1.99902 13.4624 2.53749 14.0009 3.19561 14.0009H12.7683C13.4265 14.0009 13.9649 13.4624 13.9649 12.8043V8.01794H10.7461ZM9.80015 9C9.80015 9.99411 8.99427 10.8 8.00015 10.8C7.00604 10.8 6.20015 9.99411 6.20015 9H5.00015C5.00015 10.6569 6.3433 12 8.00015 12C9.65701 12 11.0002 10.6569 11.0002 9H9.80015Z" fill="#dcddde"/></StickerTooltipIcon> {s.name}</React.Fragment>}
                           mouseEnterDelay={.25}
                           mouseLeaveDelay={0}
                         >
                           {s.formatType === 'LOTTIE' ? <span>Lottie Sticker: {s.name}</span>
-                          : <img key={i} height={160} width={160} alt={s.name+' Sticker'} src={`https://cdn.discordapp.com/stickers/${s.id}/${s.icon}.png`} draggable={false} />}
+                          : <img height={160} width={160} alt={s.name+' Sticker'} src={`https://cdn.discordapp.com/stickers/${s.id}/${s.icon}.png`} draggable={false} />}
                         </Tooltip>
                       )}
 
