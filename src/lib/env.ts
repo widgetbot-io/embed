b3428310ccaa0ac874feb2db8c941e3b7d97436b
Daave <daave@widgetbot.io>
2021-01-21 22:07:39 +0000

    ci: Hopefully fix some issue and add a fallback

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,15 +1,17 @@
 export const url = getEnvVar('{CUSTOM_SERVER_ENDPOINT}')
 
-/** 
+/**
  * When in develop mode, read env vars normally, when in production output a special string that will be replaced by script
 **/
 function getEnvVar(envVarStr: string): string {
     const overrides = {
         CUSTOM_SERVER_ENDPOINT: "s-e.widgetbot.io"
     }
+    console.log(envVarStr)
+    console.log(process.env.NODE_ENV)
 
     // is true when running: npm run build
     const isProd = process.env.NODE_ENV === 'production'
     const envVar = envVarStr.replace(/[{}]/g, '')
-    return isProd ? envVarStr : (overrides[envVar] || process.env[envVar])
+    return isProd ? (envVarStr || overrides[envVar]) : (overrides[envVar] || process.env[envVar])
 }
